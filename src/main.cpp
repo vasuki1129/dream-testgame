e893df7c4a72c91bfe0f2814236c55261d61125c
Deepfrog <froggy@deepfrog.net>
2025-10-18 20:37:23 -0400

    Updated to use new component features

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,14 @@
-#include <iostream>
 #include <dream.h>
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
-class TestObject : public GameObject
-{
-    float posX = 0.0f;
-    float posY = 0.0f;
-public:
-    void tick(float delta) override;
-    void render() override;
-
-};
-
-
-void TestObject::tick(float delta) {
-
-  if (DreamKeyDown(GLFW_KEY_W)) {
-      posY -= delta * 1.0f;
-  }
-  if (DreamKeyDown(GLFW_KEY_S)) {
-      posY += delta * 1.0f;
-  }
-
-  if (DreamKeyDown(GLFW_KEY_A)) {
-      posX -= delta * 1.0f;
-  }
-  if (DreamKeyDown(GLFW_KEY_D)) {
-      posX += delta * 1.0f;
-  }
-}
-
-void TestObject::render()
-{
-    DreamDrawQuadTexture(posX,posY,128,128,glm::vec4(1.0f,0.0f,0.0f,1.0f),"test.png");
-}
 
 int main(int argc, char *argv[]) {
 
     DreamInitFunc([]()
         {
-            DreamAddGameObject(new TestObject());
+           DreamAddGameObject(new GameObject())
+               ->AddComponent(new SpriteRenderComponent("test.png",glm::vec2(128,128)))
+               ->AddComponent(new ConstantTravelComponent(glm::vec2(1.0f,1.0f),10.0f));
         }
     );
 
